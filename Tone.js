9bc560917a59b8f01761ef8fe163367a2a7f7d37
Imkevinhsu <47855613+Imkevinhsu@users.noreply.github.com>
2019-12-16 12:46:54 +0800

    Create Tone.js
    upload

---
+++ Tone.js
@@ -0,0 +1,41 @@
+// get Tone_Analysis response 
+// must input parameter
+function get_tone_analysis(input_Tone_Analysis){
+  const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+  const { IamAuthenticator } = require('ibm-watson/auth');
+
+  // set apikey and url
+  const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  authenticator: new IamAuthenticator({
+    apikey: 'IR3urmeUo9Uft1XXq2qAAzE5L_opLhxsyVepEwivFwlt',
+  }),
+  url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
+});
+
+
+// set Tone_Analysis parameter
+  const toneParams = {
+  toneInput: { 'text': input_Tone_Analysis },
+  contentType: 'application/json',
+};
+
+  toneAnalyzer.tone(toneParams)
+  .then(toneAnalysis => {
+   data=JSON.stringify(toneAnalysis, null, 2);
+    console.log(data);
+    return data;
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+  }
+
+  // input from translate --->阿棋
+  input = 'Team, I know that times are tough! Product '
+  + 'sales have been disappointing for the past three '
+  + 'quarters. We have a competitive product, but we '
+  + 'need to do a better job of selling it!';
+
+a=get_tone_analysis(input);
+console.log(a);
