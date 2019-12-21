02f2bbda920de7fac5539a3ed94ab0783e9ea1d1
Imkevinhsu <47855613+Imkevinhsu@users.noreply.github.com>
2019-12-21 17:02:07 +0800

    Update Tone.js
    update

--- Tone.js
+++ Tone.js
@@ -1,34 +1,36 @@
 // get Tone_Analysis response 
 // must input parameter
 function get_tone_analysis(input_Tone_Analysis){
-  const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-  const { IamAuthenticator } = require('ibm-watson/auth');
+  return new Promise(function(resolve,reject){
+    const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+    const { IamAuthenticator } = require('ibm-watson/auth');
 
-  // set apikey and url
-  const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({
-    apikey: 'IR3urmeUo9Uft1XXq2qAAzE5L_opLhxsyVepEwivFwlt',
-  }),
-  url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
-});
-
-
-// set Tone_Analysis parameter
-  const toneParams = {
-  toneInput: { 'text': input_Tone_Analysis },
-  contentType: 'application/json',
-};
-
-  toneAnalyzer.tone(toneParams)
-  .then(toneAnalysis => {
-   data=JSON.stringify(toneAnalysis, null, 2);
-    console.log(data);
-    return data;
-  })
-  .catch(err => {
-    console.log('error:', err);
+    // set apikey and url
+    const toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21',
+    authenticator: new IamAuthenticator({
+      apikey: 'IR3urmeUo9Uft1XXq2qAAzE5L_opLhxsyVepEwivFwlt',
+    }),
+    url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
   });
+
+
+  // set Tone_Analysis parameter
+    const toneParams = {
+    toneInput: { 'text': input_Tone_Analysis },
+    contentType: 'application/json',
+  };
+
+    toneAnalyzer.tone(toneParams)
+    .then(toneAnalysis => {
+    data=JSON.stringify(toneAnalysis, null, 2);
+      // console.log(data);
+      resolve(data);
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
+    })
   }
 
   // input from translate --->阿棋
@@ -37,5 +39,7 @@ function get_tone_analysis(input_Tone_Analysis){
   + 'quarters. We have a competitive product, but we '
   + 'need to do a better job of selling it!';
 
-a=get_tone_analysis(input);
-console.log(a);
+
+  get_tone_analysis(input).then(function(value){
+    console.log(value);
+  })
